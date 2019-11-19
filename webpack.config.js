eaa89a98a64283901164aa1b9e7be322bdbded7b
Baboo7 <dbsharp404@gmail.com>
2019-11-19 18:14:09 -0500

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[contenthash:8].js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: [/.js$|.ts$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/typescript", "@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: [/.css$|.scss$/],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+          "postcss-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: ["node_modules", path.resolve(__dirname, "src")],
+    extensions: [".js", ".ts"]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
+    new HtmlWebpackPlugin({
+      title: "Secret Santa 🎅",
+      template: "./src/index.html",
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      }
+    })
+  ]
+};
