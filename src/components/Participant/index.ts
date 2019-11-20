18df0980bca9db9c1290c7a963b5692301db3ebd
Baboo7 <dbsharp404@gmail.com>
2019-11-19 22:49:11 -0500

    Add listener to participant on click

--- src/components/Participant/index.ts
+++ src/components/Participant/index.ts
@@ -1,5 +1,7 @@
 import "./Participant.scss";
 
+type OnClickType = (name: string) => void;
+
 class Participant {
   /**
    * Create a new participant and insert it into the DOM.
@@ -7,18 +9,23 @@ class Participant {
    * @param name {string} Name of the participant
    * @param spouseName {string | null} Name of the participant's spouse
    * @param isSelected {boolean} Whether the participant is selected
+   * @param onClick {OnClickType}
    */
   create(
     parent: HTMLElement,
     name: string,
     spouseName: string | null,
     isSelected: boolean,
+    onClick: OnClickType = () => {}
   ): void {
     // Create component elements
     const component = document.createElement("div");
     const className = ["participant"];
     if (isSelected) className.push("selected");
     component.className = className.join(" ");
+    component.addEventListener("click", () => {
+      onClick(name);
+    });
 
     const nameNode = document.createTextNode(name);
 
