125e212fe6063276e119cdee853bffedbc043e67
Baboo7 <dbsharp404@gmail.com>
2019-11-19 19:28:22 -0500

    Create participant on enter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 import "./style.scss";
+
+import { Participant } from "./components";
+
+const participantInput = document.getElementById(
+  "participant-input"
+) as HTMLInputElement;
+const participantsList = document.getElementById(
+  "participants-list"
+) as HTMLElement;
+
+participantInput.addEventListener("keydown", (ev: KeyboardEvent) => {
+  // On Enter key down
+  if (ev.keyCode === 13) {
+    const participantName = participantInput.value;
+    Participant.create(participantsList, participantName);
+    participantInput.value = "";
+  }
+});
