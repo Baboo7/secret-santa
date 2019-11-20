4fdcf3563e43177555abf9189f34dff427639021
Baboo7 <dbsharp404@gmail.com>
2019-11-19 23:54:53 -0500

    Use list page

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import "./style.scss";
 
-import { Participant } from "./components";
-import { isStrBlank } from "./services/validatorService";
-import { removeChildren } from "./services/domService";
+import { ListPage } from "./pages";
 
 /**
  * Holds participants.
@@ -10,62 +8,6 @@ import { removeChildren } from "./services/domService";
  * - value: spouse name
  */
 const participants: Map<string, string | null> = new Map();
-let selectedParticipant: string | null = null;
 
-const participantInput = document.getElementById(
-  "participant-input"
-) as HTMLInputElement;
-const participantsList = document.getElementById(
-  "participants-list"
-) as HTMLElement;
-
-participantInput.addEventListener("keydown", (ev: KeyboardEvent) => {
-  // On Enter key down
-  if (ev.keyCode === 13) {
-    const participantName = participantInput.value.trim();
-    if (participants.has(participantName) || isStrBlank(participantName)) {
-      return;
-    }
-
-    participants.set(participantName, null);
-    participantInput.value = "";
-    renderParticipantList();
-  }
-});
-
-const renderParticipantList = (): void => {
-  removeChildren(participantsList);
-  participants.forEach((spouseName: string | null, participantName: string) => {
-    const isSelected = selectedParticipant === participantName;
-
-    Participant.create(
-      participantsList,
-      participantName,
-      spouseName,
-      isSelected,
-      handleParticipantClicked
-    );
-  });
-};
-
-const handleParticipantClicked = (name: string): void => {
-  if (selectedParticipant === null) {
-    selectedParticipant = name;
-  } else if (selectedParticipant === name) {
-    selectedParticipant = null;
-  } else {
-    // Remove old spouses
-    const selectedParticipantSpouse = participants.get(selectedParticipant);
-    if (selectedParticipantSpouse)
-      participants.set(selectedParticipantSpouse, null);
-    const nameSpouse = participants.get(name);
-    if (nameSpouse) participants.set(nameSpouse, null);
-
-    // Set new spouses
-    participants.set(selectedParticipant, name);
-    participants.set(name, selectedParticipant);
-    selectedParticipant = null;
-  }
-
-  renderParticipantList();
-};
+const listPage = new ListPage(participants);
+listPage.build();
