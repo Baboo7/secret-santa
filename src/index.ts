6a9a33768e1caf9687b5ebd3c6648790028760f9
Baboo7 <dbsharp404@gmail.com>
2019-11-19 22:50:24 -0500

    Handle click on participant

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { removeChildren } from "./services/domService";
  * - value: spouse name
  */
 const participants: Map<string, string | null> = new Map();
+let selectedParticipant: string | null = null;
 
 const participantInput = document.getElementById(
   "participant-input"
@@ -34,8 +35,37 @@ participantInput.addEventListener("keydown", (ev: KeyboardEvent) => {
 
 const renderParticipantList = (): void => {
   removeChildren(participantsList);
-  participants.forEach((_: string | null, key: string) => {
-    Participant.create(participantsList, key, handleParticipantClicked);
+  participants.forEach((spouseName: string | null, participantName: string) => {
+    const isSelected = selectedParticipant === participantName;
+
+    Participant.create(
+      participantsList,
+      participantName,
+      spouseName,
+      isSelected,
+      handleParticipantClicked
+    );
   });
 };
 
+const handleParticipantClicked = (name: string): void => {
+  if (selectedParticipant === null) {
+    selectedParticipant = name;
+  } else if (selectedParticipant === name) {
+    selectedParticipant = null;
+  } else {
+    // Remove old spouses
+    const selectedParticipantSpouse = participants.get(selectedParticipant);
+    if (selectedParticipantSpouse)
+      participants.set(selectedParticipantSpouse, null);
+    const nameSpouse = participants.get(name);
+    if (nameSpouse) participants.set(nameSpouse, null);
+
+    // Set new spouses
+    participants.set(selectedParticipant, name);
+    participants.set(name, selectedParticipant);
+    selectedParticipant = null;
+  }
+
+  renderParticipantList();
+};
